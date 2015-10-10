c9c412f4c975cacedcfd6554bcec323bc285dc1a
tommylavoie <tom.patate@hotmail.com>
2015-10-10 11:02:05 -0400

    Menu choix debut

--- WonderJamA2015/Assets/Scripts/UI/Menu.cs
+++ WonderJamA2015/Assets/Scripts/UI/Menu.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Menu : MonoBehaviour
 {
+    public List<GameObject> faces;
+    int actualCursor = 0;
     int step = 0;
+    bool changingCharacter = false;
     // Use this for initialization
     void Start ()
     {
@@ -23,9 +26,56 @@ public class Menu : MonoBehaviour
         {
             if(step == 0)
             {
-                Text startText = GetComponentInChildren<Text>();
+                Text startText = GameObject.FindGameObjectWithTag("Respawn").GetComponentInChildren<Text>();
+                Text choiceText = GameObject.FindGameObjectWithTag("Finish").GetComponentInChildren<Text>();
                 startText.enabled = false;
+                choiceText.enabled = true;
+                foreach(GameObject obj in faces)
+                {
+                    obj.SetActive(true);
+                }
+                SpriteRenderer fleche = (SpriteRenderer)GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
+                fleche.enabled = true;
+                moveCursor();
+                step = 1;
             }
         }
+        if (step == 1)
+        {
+            if (Input.GetAxis("Horizontal") > 0 && !changingCharacter)
+            {
+                changingCharacter = true;
+                nextCharacter();
+            }
+            else if (Input.GetAxis("Horizontal") < 0 && !changingCharacter)
+            {
+                changingCharacter = true;
+                precCharacter();
+            }
+            else if(Input.GetAxis("Horizontal") == 0 && changingCharacter)
+            {
+                changingCharacter = false;
+            }
+        }
+    }
+
+    void nextCharacter()
+    {
+        if(actualCursor < faces.Count-1)
+            actualCursor++;
+        moveCursor();  
+    }
+
+    void precCharacter()
+    {
+        if (actualCursor > 0)
+            actualCursor--;
+        moveCursor();
+    }
+
+    void moveCursor()
+    {
+        GameObject fleche = GameObject.FindGameObjectWithTag("Player");
+        fleche.transform.position = new Vector3(faces[actualCursor].transform.position.x, faces[actualCursor].transform.position.y + 3);
     }
 }
